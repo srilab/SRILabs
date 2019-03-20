4db8d74efeb4a3e299c0ec9140b81dbc7d515bd8
Abigail Oswald <abigailm.oswald@gmail.com>
2019-03-20 03:41:32 +0000

    add delete blog functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,8 @@
-window.onload = function() {
+function clearBlogContent() {
     var blogContent = document.getElementById("blogContent");
-    if(blogContent) {
-        blogContent.addEventListener("click", function() {
-            console.log("hello");
-            if(this.value === "  Blog Post Goes Here") {
-                this.value = "";
-            }
-        });
+    if(blogContent && blogContent.value === "  Blog Post Goes Here") {
+        blogContent.value = "";
     }
 }
+
+window.onload = clearBlogContent(); 
--- routes/index.js
+++ routes/index.js
@@ -124,4 +124,27 @@ router.get("/blog/:id/edit", isLoggedIn, checkUserPost, function(req, res){
   res.render("blog/edit", {post: req.post, page: 'blog'});
 });
 
+// DELETE - removes campground and its comments from the database
+router.delete("/blog/:id", isLoggedIn, checkUserPost, function(req, res) {
+    Post.remove({
+      _id: {
+        $in: req.post.comments
+      }
+    }, function(err) {
+      if(err) {
+          req.flash('error', err.message);
+          res.redirect('/');
+      } else {
+          req.post.remove(function(err) {
+            if(err) {
+                req.flash('error', "Whoops! " + err.message + ".");
+                return res.redirect('/');
+            }
+            req.flash('success', 'Successfully Deleted Blog Post.');
+            res.redirect('/blog');
+          });
+      }
+    });
+});
+
 module.exports = router;
